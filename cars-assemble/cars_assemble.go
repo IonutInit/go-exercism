d4ae57c470dd7090bd9103069704be83038f9c8f
IonutInit <ionutcojocaru@outlook.com>
2023-08-18 18:03:08 +0100

    exercises

---
+++ cars-assemble/cars_assemble.go
@@ -0,0 +1,20 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * successRate / 100
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	mod := carsCount % 10
+	whole := carsCount - mod
+	return (uint)((whole * 9500) + (mod * 10000))
+}
